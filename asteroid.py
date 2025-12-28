22da94c0e12395f5bdc1c55ce8c8fa562a0f34cb
github-zy <843019013@qq.com>
2025-12-28 23:07:15 +0800

    spilit

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
 import pygame
+import random
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from logger import log_event
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -11,3 +13,18 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        log_event("asteroid_split")
+        random_rotation = random.uniform(20,30)
+        new_velocity_1 = self.velocity.rotate(random_rotation)
+        new_velocity_2 = self.velocity.rotate(-random_rotation)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        smaller_asteroid_1 = Asteroid(self.position.x, self.position.y, new_radius)
+        smaller_asteroid_2 = Asteroid(self.position.x, self.position.y, new_radius)
+        smaller_asteroid_1.velocity = new_velocity_1 * 1.2
+        smaller_asteroid_2.velocity = new_velocity_2 * 1.2
+        
